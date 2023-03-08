4047ed08f707469c1001f569cc2316f2f54d3998
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-03-08 15:29:10 -0700

    Small clean up

--- DesignPatterns/src/Main.java
+++ DesignPatterns/src/Main.java
@@ -1,18 +1,6 @@
-import Creational.Builder.MovieBuilder;
-import Creational.Builder.PdfDocumentBuilder;
-import Creational.Builder.Presentation;
-import Creational.Builder.Slide;
 
 public class Main {
     public static void main(String[] args) {
-        var presentation = new Presentation();
-        presentation.addSlide(new Slide("Slide 1"));
-        presentation.addSlide(new Slide("Slide 2"));
-        presentation.addSlide(new Slide("Slide 3"));
-        presentation.addSlide(new Slide("Slide 4"));
 
-        var builder = new MovieBuilder();
-        presentation.export(builder);
-        var pdf = builder.getMovie();
     }
 }
