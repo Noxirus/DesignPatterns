dfd5ef591e58edd3dbf7e7e773d7b2974e79553e
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2023-03-14 22:22:22 -0600

    Added more linked list logic

--- Algorithms/JavaProject/src/LinkedList/LinkedList.java
+++ Algorithms/JavaProject/src/LinkedList/LinkedList.java
@@ -1,70 +1,135 @@
 package LinkedList;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
     private Node first;
     private Node last;
 
+    private int size;
+
+    public void reverse(){
+        if(isEmpty()) return;
+
+       var previous = first;
+       var current = first.getNext();
+       while(current != null){
+           var next = current.getNext();
+           current.setNext(previous);
+           previous = current;
+           current = next;
+       }
+
+       last = first;
+       last.setNext(null);
+       first = previous;
+    }
+
+    public int getKthFromTheEnd(int k){
+        if(isEmpty()){
+            throw new IllegalStateException();
+        }
+
+        if(k <= 0){
+            throw new IllegalArgumentException();
+        }
+
+        int distance = k - 1;
+        Node firstNode = first;
+        Node secondNode = first;
+
+        int step = 0;
+        while(firstNode.getNext() != null){
+            step++;
+            if(step > distance){
+                secondNode = secondNode.getNext();
+            }
+            firstNode = firstNode.getNext();
+        }
+
+        if(step < distance){
+            return -1;
+        }
+
+        return secondNode.getValue();
+    }
+
     //addFirst
     public void addFirst(int value){
-        if(first == null){
-            first = new Node(value);
+        Node newNode = new Node(value);
+        if(isEmpty()){
+            first = newNode;
             last = first;
         }
         else{
-            Node newNode = new Node(value);
             newNode.setNext(first);
             first = newNode;
         }
+
+        size++;
     }
 
     //addLast
     public void addLast(int value){
-        if(first == null){
-            first = new Node(value);
+        Node newNode = new Node(value);
+        if(isEmpty()){
+            first = newNode;
             last = first;
         }
         else{
-            Node newNode = new Node(value);
             last.setNext(newNode);
             last = newNode;
         }
+
+        size++;
     }
 
     //deleteFirst
     public void deleteFirst(){
-        if(first.getNext() == null){
-            last = null;
+        if(isEmpty()){
+            throw new NoSuchElementException();
         }
-        first = first.getNext();
+
+        if(first == last){
+            first = last = null;
+        }
+        else{
+            var second = first.getNext();
+            first.setNext(null);
+            first = second;
+        }
+        size--;
     }
     //deleteLast
     public void deleteLast(){
-        Node currentNode = first;
-        Node previousNode = null;
-        while(currentNode != null){
-            if(currentNode.getNext() == null){
-                System.out.println("Current: " + currentNode.getValue() + " / Previous: " + previousNode.getValue());
-                last = previousNode;
-                last.setNext(null);
-                return;
-            }
-            else{
-                previousNode = currentNode;
-                currentNode = currentNode.getNext();
-            }
+        if(isEmpty()){
+            throw new NoSuchElementException();
         }
+
+        if(first == last){
+            first = last = null;
+        }
+        else{
+            var previous = getPrevious(last);
+            last = previous;
+            last.setNext(null);
+        }
+        size--;
+    }
+
+    private Node getPrevious(Node node){
+        var current = first;
+        while(current != null){
+            if(current.getNext() == node){
+                return current;
+            }
+            current = current.getNext();
+        }
+        return null;
     }
     //contains
     public boolean contains(int value){
-        Node currentNode = first;
-        while(currentNode != null){
-            if(currentNode.getValue() == value){
-                return true;
-            }
-            currentNode = currentNode.getNext();
-        }
-
-        return false;
+        return(indexOf(value)) != -1;
     }
     //indexOf
     public int indexOf(int index){
@@ -91,5 +156,26 @@ public class LinkedList {
         }
         string.append("]");
         return string.toString();
+    }
+
+    public int[] toArray(){
+        int[] array = new int[size];
+        int count = 0;
+        Node currentNode = first;
+
+        while(currentNode != null){
+            array[count++] = currentNode.getValue();
+            currentNode = currentNode.getNext();
+        }
+
+        return array;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    private boolean isEmpty(){
+        return first == null;
     }
 }
--- Algorithms/JavaProject/src/Main.java
+++ Algorithms/JavaProject/src/Main.java
@@ -3,15 +3,13 @@ import LinkedList.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
-        list.addFirst(1);
-        list.addFirst(2);
-        list.addFirst(3);
-        list.addFirst(4);
-        list.addFirst(5);
-        list.addLast(6);
-        list.deleteLast();
-        list.deleteLast();
-        System.out.println(list.returnList());
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
 
+        System.out.println(list.returnList());
+        System.out.println(list.getKthFromTheEnd(0));
     }
 }
